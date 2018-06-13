83d3b50b579fa7bd0615cceaf20f84bb413ff896
mike <mikemay@blueyonder.co.uk>
2018-06-13 11:49:29 +0100

    shows tuples displaying incorrectly

---
+++ SandDiag/IFoo.cs
@@ -0,0 +1,19 @@
+﻿namespace SandDiag
+{
+    /// <summary>
+    /// summary of IFoo
+    /// </summary>
+    public interface IFoo
+    {
+        /// <summary>
+        /// summary of Bar
+        /// </summary>
+        /// <param name="someTuple">s/be an int and a string</param>
+        void Bar((int, string) someTuple);
+        /// <summary>
+        /// summary of Qux
+        /// </summary>
+        /// <returns>s/be a (int, string) or a ValueTuple&lt;int, string></returns>
+        (int, string) Qux();
+    }
+}
